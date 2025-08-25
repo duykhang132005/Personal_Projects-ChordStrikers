2ded881ceb7607904f2f7084804a6bf4d59709ba
KHANG NGUYEN <KHANGND01@GMAIL.COM>
2025-08-25 13:20:37 -0400

    Remove obsolete ChordStrikers folder after restructuring

--- ChordStrikers/app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///songs.db"  # Flask will resolve this inside 'instance/'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- ChordStrikers/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-import os
-
-app = create_app()
-app.secret_key = os.environ.get('SECRET_KEY', 'fallback-dev-key')
-
-if __name__ == "__main__":
-    app.run(debug=True)
